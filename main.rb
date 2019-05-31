f36671e43e1cc8441a19a0aa6afd46caf5140aa4
Ashish Kamath <ashish.kamath@joolhealth.com>
2019-05-31 14:08:58 -0400

    Added services/providers list

--- main.rb
+++ main.rb
@@ -28,41 +28,79 @@ end
 # change all argv[0] to response
 while response != 'exit'.downcase
 
-
-
   if response == 'service:add'
     name = prompt.ask('name:')
     price = prompt.ask('price:')
     length = prompt.ask('length:')
     service_list.push(Service.new(name, price, length))
     puts "New service successfully added!"
-    puts "Here is your new service list:"
-    puts service_list.inspect
+    puts "Here is your new service list:
+          ------------------------------"
+    service_list.each do |service|
+      puts "
+            Service: #{service.name}
+            Price: $#{service.price}
+            Length: #{service.length} hrs
+           "
+    end
+    puts "
+          ------------------------------"
+
   elsif response == 'service:delete'
     name = prompt.ask('name:')
     service_list.delete_if {|item| item.name == "#{name}"}
     puts "Service successfully deleted!"
-    puts "Here is your new service list:"
-    puts service_list.inspect
+    puts "Here is your new service list:
+          ------------------------------"
+    service_list.each do |service|
+      puts "
+            Service: #{service.name}
+            Price: $#{service.price}
+            Length: #{service.length} hrs
+           "
+    end
+    puts "
+          ------------------------------"
+
   elsif response == 'provider:add'
     name = prompt.ask('name:')
-    service = prompt.ask('service:')
+    service = prompt.ask('services:')
     phone = prompt.ask('phone number:')
     provider_list.push(Provider.new(name, service, phone))
     puts "Service provider successfully added!"
-    puts "Here is your new provider list:"
-    puts provider_list.inspect
+    puts "Here is your new provider list:
+          ------------------------------"
+    provider_list.each do |provider|
+      puts "
+            Provider: #{provider.name}
+            Services: #{provider.service}
+            Phone Number: #{provider.phone}
+           "
+    end
+    puts "
+          ------------------------------"
+
   elsif response == 'provider:delete'
     name = prompt.ask('name:')
     provider_list.delete_if {|item| item.name == "#{name}" }
     puts "Service provider successfully deleted!"
-    puts "Here is your new provider list:"
-    puts provider_list.inspect
+    puts "Here is your new provider list:
+          ------------------------------"
+    provider_list.each do |provider|
+      puts "
+            Provider: #{provider.name}
+            Services: #{provider.service}
+            Phone Number: #{provider.phone}
+           "
+    end
+    puts "
+          ------------------------------"
+
   elsif response == 'appointment:add'
     name_client = prompt.ask('client name:')
     name_service = prompt.ask('service name:')
     one_aptmt = prompt.ask('is this one time appointment?')
-    if one_aptmt == 'Y'/'y'
+    if one_aptmt == 'Y' || 'y'
       # puts open time slots here
       time_client = prompt.ask('choose one of the above slot')
     end
