acca7aba66028029f1959f864caec9eb757ef599
Kai Cui <cuikai@umich.edu>
2019-05-31 11:19:57 -0400

    ood

--- main.rb
+++ main.rb
@@ -4,44 +4,68 @@ prompt = TTY::Prompt.new
 
 service_list =  []
 service_list.push(Service.new('spiritual healing', 200, 1))
+service_list.push(Service.new('yoga', 300, 1))
+service_list.push(Service.new('swimming',200,1))
 
 provider_list = []
 provider_list.push(Provider.new('Kai', 'spiritual healing', 7349908790))
+provider_list.push(Provider.new('Ashish', 'yoga, spiritual healing', 7890029087))
 
 if !ARGV[0]
   puts "Welcome to Hacker Fellows. Please use the following commands: "
   puts "
-        service:add,
-        service:delete,
+        service:add
+        service:delete
         provider:add
         provider:delete
         appointment:add
         appointment:list
+        exit
        "
+  response = prompt.ask("enter a command")
 end
 
-if ARGV[0] == 'service:add'
-  name = prompt.ask('name:')
-  price = prompt.ask('price:')
-  length = prompt.ask('length:')
-  service_list.push(Service.new(name, price, length))
-elsif ARGV[0] == 'service:delete'
-  name = prompt.ask('name:')
-  service_list.delete_if {|item| item.name == "#{name}"}
-elsif ARGV[0] == 'provider:add'
-  name = prompt.ask('name:')
-  service = prompt.ask('service:')
-  phone = prompt.ask('phone number:')
-  provider_list.push(Provider.new(name, service, phone))
-elsif ARGV[0] == 'provider:delete'
-  name = prompt.ask('name:')
-  provider_list.delete_if {|item| item.name == "#{name}" }
-elsif ARGV[0] == 'appointment:add'
-  name_client = prompt.ask('client name:')
-  name_service = prompt.ask('service name:')
-  one_aptmt = prompt.ask('is this one time appointment?')
-  if one_aptmt == 'Y'/'y'
-    # puts open time slots here
-    time_client = prompt.ask('choose one of the above slot')
+# change all argv[0] to response
+while response != 'exit'.downcase
+
+
+
+  if response == 'service:add'
+    name = prompt.ask('name:')
+    price = prompt.ask('price:')
+    length = prompt.ask('length:')
+    service_list.push(Service.new(name, price, length))
+    puts "New service successfully added!"
+    puts "Here is your new service list:"
+    puts service_list.inspect
+  elsif response == 'service:delete'
+    name = prompt.ask('name:')
+    service_list.delete_if {|item| item.name == "#{name}"}
+    puts "Service successfully deleted!"
+    puts "Here is your new service list:"
+    puts service_list.inspect
+  elsif response == 'provider:add'
+    name = prompt.ask('name:')
+    service = prompt.ask('service:')
+    phone = prompt.ask('phone number:')
+    provider_list.push(Provider.new(name, service, phone))
+    puts "Service provider successfully added!"
+    puts "Here is your new provider list:"
+    puts provider_list.inspect
+  elsif response == 'provider:delete'
+    name = prompt.ask('name:')
+    provider_list.delete_if {|item| item.name == "#{name}" }
+    puts "Service provider successfully deleted!"
+    puts "Here is your new provider list:"
+    puts provider_list.inspect
+  elsif response == 'appointment:add'
+    name_client = prompt.ask('client name:')
+    name_service = prompt.ask('service name:')
+    one_aptmt = prompt.ask('is this one time appointment?')
+    if one_aptmt == 'Y'/'y'
+      # puts open time slots here
+      time_client = prompt.ask('choose one of the above slot')
+    end
   end
+  response = prompt.ask("enter a command")
 end
--- menue.rb
+++ menue.rb
@@ -28,3 +28,44 @@ puts menu.map { |key, value|
 # menu.keys.each do |item|
 #   puts item.ljust(l, '.')
 # end
+class Appointment
+  def initialize(start_time, length_in_minute)
+    @start_time = start_time
+    @length_in_minute = length_in_minute
+  end
+
+  def instance_variable_test
+    @start_time
+  end
+
+  def local_variable_test
+    start_time
+  end
+end
+
+appointment = Appointment.new('2:00', 60)
+
+class Appointment1
+  # read info from characters
+  attr_reader :start_time, :length_in_minute
+  # enable you to rewrite values in these characters
+  attr_writer :start_time, :length_in_minute
+  # both read and write
+  attr_accessor :start_time, :length_in_minute
+
+  def initialize(start_time, length_in_minute)
+    @start_time = start_time
+    @length_in_minute = length_in_minute
+  end
+
+  # instance method, inside this class
+  def end_time
+    @start_time + @length_in_minute
+  end
+
+  def self.shift_forward(appointments)
+    appointments.map do |appointment|
+      appointment.start_time
+    end
+  end
+end
